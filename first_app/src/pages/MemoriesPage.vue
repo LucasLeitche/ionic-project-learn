f7c50a76dde5f9fea4d427db188fac876d7f4cab
LucasLeitche <llneves019@gmail.com>
2022-03-22 23:50:14 -0300

    New Commit update

---
+++ first_app/src/pages/MemoriesPage.vue
@@ -0,0 +1,32 @@
+<template>
+    <ion-page>
+        <ion-header >
+            <ion-toolbar>
+                <ion-title>All Memories</ion-title>
+            </ion-toolbar>
+        </ion-header> 
+        <ion-content>
+            <ion-list>
+                <ion-item>Max</ion-item>
+                <ion-item>Manuel</ion-item>
+                <ion-item>Jully</ion-item>
+                <ion-item>Jorge</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonTitle, IonContent, IonToolbar, IonList, IonItem} from '@ionic/vue';
+export default {
+    components: {
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonContent,
+        IonToolbar,
+        IonList,
+        IonItem
+    }
+}
+</script>
