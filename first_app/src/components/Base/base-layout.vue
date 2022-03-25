11f11444edb1b1db6ffe9ada72ea48bedb578eac
LucasLeitche <llneves019@gmail.com>
2022-03-25 17:02:26 -0300

    Upgrade

---
+++ first_app/src/components/Base/base-layout.vue
@@ -0,0 +1,42 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-buttons slot="start"> <!--Usado slot para caso desejado utilizar mais de um botão no header-->
+                    <ion-back-button :default-href="defaultPage" ></ion-back-button>
+                </ion-buttons>
+                <ion-title>{{title}}</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <slot></slot>
+        </ion-content>
+    </ion-page>
+</template>
+
+<script>
+import {IonPage, IonHeader, IonTitle, IonContent, IonToolbar, IonButtons, IonBackButton} from '@ionic/vue'
+export default {
+    props: {
+        title:{
+            type:String
+        },
+        defaultPage:{
+            type: String
+        }
+    },
+    components:{
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonContent,
+        IonToolbar,
+        IonButtons,
+        IonBackButton
+    }
+}
+</script>
+
+<style>
+
+</style>
